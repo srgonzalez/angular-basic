1ca8e56b99d5f6439e8723f3df227b1841734d38
Sergio Rodriguez <srg301091@gmail.com>
2022-11-12 16:51:55 -0500

    Subida del proyecto

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Thor', 'Hulk', 'Capitan America'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
